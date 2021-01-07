73bd1e5fb17567192e8dafdab2a7837dc677072e
Khushbu <khushbukellan@gmail.com>
2021-01-07 17:09:45 +0530

    Added an awesome comment

--- my-awesome-script.py
+++ my-awesome-script.py
@@ -1,5 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Howdy, World")
+#This is an awesome Python script
 =======
 print("Hajimemashite sekai!")
+>>>>>>> feature/enhancement-2
+=======
+print("Howdy, World")
 >>>>>>> feature/enhancement-2
